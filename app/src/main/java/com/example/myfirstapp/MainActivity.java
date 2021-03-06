13a72cddca79408ccd1bba27fdc78fb24510cb30
pilan0105 <76063789+pilan0105@users.noreply.github.com>
2021-03-06 18:14:08 +0100

    methode asci konvertierung erstellt

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
    EditText matno;
@@ -43,10 +45,64 @@ public class MainActivity extends AppCompatActivity {
             modifiedSentence = inFromServer.readLine();
             System.out.println("Vom Server erhalten:" + modifiedSentence);
             TextView text = findViewById(R.id.textView3);
+            text.setGravity(Gravity.CENTER);
             text.setText(modifiedSentence);
-        clientSocket.close();}
+            String test= converttoasci(sentece);
+            System.out.println(test);
+            clientSocket.close();
+        }
         catch(IOException e){
             e.printStackTrace();
         }
+    }
+    
+    public String converttoasci(String matno){
+        String result="";
+        char [] chararray = matno.toCharArray();
+        System.out.println(chararray.length);
+        if(chararray.length!=8){
+            result= "Dies ist keine gültige Matrikelnummer!";
+        } else{
+        for(int i = 0; i<chararray.length; i = i+2){
+            switch(chararray[i]){
+                case '0':
+                    chararray[i]='a';
+                    break;
+                case '1':
+                    chararray[i]='b';
+                    break;
+                case '2':
+                    chararray[i]='c';
+                    break;
+                case '3':
+                    chararray[i]='d';
+                    break;
+                case '4':
+                    chararray[i]='e';
+                    break;
+                case '5':
+                    chararray[i]='f';
+                    break;
+                case '6':
+                    chararray[i]='g';
+                    break;
+                case '7':
+                    chararray[i]='h';
+                    break;
+                case '8':
+                    chararray[i]='i';
+                    break;
+                case '9':
+                    chararray[i]='j';
+                    break;
+                default:
+                    break;
+
+            }
+        }
+        result = Arrays.toString(chararray);
+
+        }
+        return result;
     }
 }
